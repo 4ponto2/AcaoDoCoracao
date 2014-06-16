a15e58f73b00b25ec4ab4aed91f4f2461d5cd7f4
Marcio Lorran <fealaure@hotmail.com>
2014-06-16 16:58:00 -0300

    Inclusão de todas as texturas

--- src/com/quatropdois/acaodocoracao/TexturaActivity.java
+++ src/com/quatropdois/acaodocoracao/TexturaActivity.java
@@ -1,13 +1,10 @@
 package com.quatropdois.acaodocoracao;
 
 
-import org.w3c.dom.Text;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.text.InputFilter.LengthFilter;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -72,6 +69,89 @@ public class TexturaActivity extends Activity
         Intent iin = getIntent();
         Bundle extras = iin.getExtras();
         coracao = (int) extras.getInt("coracao");
+        
+        switch (coracao){
+        case 1:
+    		viewIMG1 .setVisibility(View.VISIBLE);
+    		viewIMG2 .setVisibility(View.INVISIBLE);
+    		viewIMG3 .setVisibility(View.INVISIBLE);
+    		viewIMG4 .setVisibility(View.INVISIBLE);
+    		viewIMG5 .setVisibility(View.INVISIBLE);
+    		viewIMG6 .setVisibility(View.INVISIBLE);
+    		viewIMG7 .setVisibility(View.INVISIBLE);
+    		viewIMG8 .setVisibility(View.INVISIBLE);
+        
+        case 2:
+    		viewIMG1 .setVisibility(View.INVISIBLE);
+    		viewIMG2 .setVisibility(View.VISIBLE);
+    		viewIMG3 .setVisibility(View.INVISIBLE);
+    		viewIMG4 .setVisibility(View.INVISIBLE);
+    		viewIMG5 .setVisibility(View.INVISIBLE);
+    		viewIMG6 .setVisibility(View.INVISIBLE);
+    		viewIMG7 .setVisibility(View.INVISIBLE);
+    		viewIMG8 .setVisibility(View.INVISIBLE);
+    		
+        case 3:
+    		viewIMG1 .setVisibility(View.INVISIBLE);
+    		viewIMG2 .setVisibility(View.INVISIBLE);
+    		viewIMG3 .setVisibility(View.VISIBLE);
+    		viewIMG4 .setVisibility(View.INVISIBLE);
+    		viewIMG5 .setVisibility(View.INVISIBLE);
+    		viewIMG6 .setVisibility(View.INVISIBLE);
+    		viewIMG7 .setVisibility(View.INVISIBLE);
+    		viewIMG8 .setVisibility(View.INVISIBLE);
+    		
+        case 4:
+    		viewIMG1 .setVisibility(View.INVISIBLE);
+    		viewIMG2 .setVisibility(View.INVISIBLE);
+    		viewIMG3 .setVisibility(View.INVISIBLE);
+    		viewIMG4 .setVisibility(View.VISIBLE);
+    		viewIMG5 .setVisibility(View.INVISIBLE);
+    		viewIMG6 .setVisibility(View.INVISIBLE);
+    		viewIMG7 .setVisibility(View.INVISIBLE);
+    		viewIMG8 .setVisibility(View.INVISIBLE);
+    		
+        case 5:
+    		viewIMG1 .setVisibility(View.INVISIBLE);
+    		viewIMG2 .setVisibility(View.INVISIBLE);
+    		viewIMG3 .setVisibility(View.INVISIBLE);
+    		viewIMG4 .setVisibility(View.INVISIBLE);
+    		viewIMG5 .setVisibility(View.VISIBLE);
+    		viewIMG6 .setVisibility(View.INVISIBLE);
+    		viewIMG7 .setVisibility(View.INVISIBLE);
+    		viewIMG8 .setVisibility(View.INVISIBLE);
+    		
+        case 6:
+    		viewIMG1 .setVisibility(View.INVISIBLE);
+    		viewIMG2 .setVisibility(View.INVISIBLE);
+    		viewIMG3 .setVisibility(View.INVISIBLE);
+    		viewIMG4 .setVisibility(View.INVISIBLE);
+    		viewIMG5 .setVisibility(View.INVISIBLE);
+    		viewIMG6 .setVisibility(View.VISIBLE);
+    		viewIMG7 .setVisibility(View.INVISIBLE);
+    		viewIMG8 .setVisibility(View.INVISIBLE);
+    		
+        case 7:
+    		viewIMG1 .setVisibility(View.INVISIBLE);
+    		viewIMG2 .setVisibility(View.INVISIBLE);
+    		viewIMG3 .setVisibility(View.INVISIBLE);
+    		viewIMG4 .setVisibility(View.INVISIBLE);
+    		viewIMG5 .setVisibility(View.INVISIBLE);
+    		viewIMG6 .setVisibility(View.INVISIBLE);
+    		viewIMG7 .setVisibility(View.VISIBLE);
+    		viewIMG8 .setVisibility(View.INVISIBLE);
+    		
+        case 8:
+    		viewIMG1 .setVisibility(View.INVISIBLE);
+    		viewIMG2 .setVisibility(View.INVISIBLE);
+    		viewIMG3 .setVisibility(View.INVISIBLE);
+    		viewIMG4 .setVisibility(View.INVISIBLE);
+    		viewIMG5 .setVisibility(View.INVISIBLE);
+    		viewIMG6 .setVisibility(View.INVISIBLE);
+    		viewIMG7 .setVisibility(View.INVISIBLE);
+    		viewIMG8 .setVisibility(View.VISIBLE);
+    		
+        }
         
     }
     
