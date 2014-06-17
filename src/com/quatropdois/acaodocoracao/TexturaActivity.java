c367b60389ae530748212d62acb41aad8f9cf303
Ismael <Ismael@192.168.0.102>
2014-06-17 10:41:06 -0300

    texturas ok

--- src/com/quatropdois/acaodocoracao/TexturaActivity.java
+++ src/com/quatropdois/acaodocoracao/TexturaActivity.java
@@ -1,11 +1,7 @@
 package com.quatropdois.acaodocoracao;
 
-
-import com.quatropdois.acaodocoracao.R.drawable;
-
 import android.app.Activity;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
@@ -19,18 +15,11 @@ public class TexturaActivity extends Activity
     public TextView t;
     
     private int coracao = 0;
+    private int textura = 0;
     
     sharedPreference qtdUso;
     
-	public ImageView	viewCor,
-						viewIMG1, 
-						viewIMG2, 
-						viewIMG3, 
-						viewIMG4, 
-						viewIMG5, 
-						viewIMG6, 
-						viewIMG7, 
-						viewIMG8;
+	public ImageView	viewCor;
 	
 	Texturas texturas;
     
@@ -44,31 +33,12 @@ public class TexturaActivity extends Activity
     }
     public void init()
     {
-//    	main = new Intent(getApplicationContext(), Main.class);
     	
         wasExecuted = false;
         
         qtdUso = new sharedPreference(getApplicationContext());
 
         viewCor  = (ImageView)findViewById(R.id.img_cor);
-        
-        viewIMG1 = (ImageView)findViewById(R.id.img1);
-        
-        viewIMG2 = (ImageView)findViewById(R.id.img2);
-        
-        viewIMG3 = (ImageView)findViewById(R.id.img3);
-        
-        viewIMG4 = (ImageView)findViewById(R.id.img4);
-        
-        viewIMG5 = (ImageView)findViewById(R.id.img5);
-        
-        viewIMG6 = (ImageView)findViewById(R.id.img6);
-        
-        viewIMG7 = (ImageView)findViewById(R.id.img7);
-        
-        viewIMG8 = (ImageView)findViewById(R.id.img8);
-        
-//        qtd = qtdUso.getInt("qtduso");
         
         Intent iin = getIntent();
         Bundle extras = iin.getExtras();
@@ -78,104 +48,40 @@ public class TexturaActivity extends Activity
         case 1:
         	viewCor.setImageResource(R.drawable.img1);
         	viewCor.setVisibility(View.VISIBLE);
-//    		viewIMG1 .setVisibility(View.VISIBLE);
-//    		viewIMG2 .setVisibility(View.INVISIBLE);
-//    		viewIMG3 .setVisibility(View.INVISIBLE);
-//    		viewIMG4 .setVisibility(View.INVISIBLE);
-//    		viewIMG5 .setVisibility(View.INVISIBLE);
-//    		viewIMG6 .setVisibility(View.INVISIBLE);
-//    		viewIMG7 .setVisibility(View.INVISIBLE);
-//    		viewIMG8 .setVisibility(View.INVISIBLE);
 			break;
         case 2:
         	viewCor.setImageResource(R.drawable.img2);
         	viewCor.setVisibility(View.VISIBLE);
-//    		viewIMG1 .setVisibility(View.INVISIBLE);
-//    		viewIMG2 .setVisibility(View.VISIBLE);
-//    		viewIMG3 .setVisibility(View.INVISIBLE);
-//    		viewIMG4 .setVisibility(View.INVISIBLE);
-//    		viewIMG5 .setVisibility(View.INVISIBLE);
-//    		viewIMG6 .setVisibility(View.INVISIBLE);
-//    		viewIMG7 .setVisibility(View.INVISIBLE);
-//    		viewIMG8 .setVisibility(View.INVISIBLE);
         	break;
         	
         case 3:
         	viewCor.setImageResource(R.drawable.img3);
-        	viewCor.setVisibility(View.VISIBLE);
-//    		viewIMG1 .setVisibility(View.INVISIBLE);
-//    		viewIMG2 .setVisibility(View.INVISIBLE);
-//    		viewIMG3 .setVisibility(View.VISIBLE);
-//    		viewIMG4 .setVisibility(View.INVISIBLE);
-//    		viewIMG5 .setVisibility(View.INVISIBLE);
-//    		viewIMG6 .setVisibility(View.INVISIBLE);
-//    		viewIMG7 .setVisibility(View.INVISIBLE);
-//    		viewIMG8 .setVisibility(View.INVISIBLE);        	
+        	viewCor.setVisibility(View.VISIBLE);        	
         	break;
         	
         case 4:
         	viewCor.setImageResource(R.drawable.img4);
         	viewCor.setVisibility(View.VISIBLE);
-//    		viewIMG1 .setVisibility(View.INVISIBLE);
-//    		viewIMG2 .setVisibility(View.INVISIBLE);
-//    		viewIMG3 .setVisibility(View.INVISIBLE);
-//    		viewIMG4 .setVisibility(View.VISIBLE);
-//    		viewIMG5 .setVisibility(View.INVISIBLE);
-//    		viewIMG6 .setVisibility(View.INVISIBLE);
-//    		viewIMG7 .setVisibility(View.INVISIBLE);
-//    		viewIMG8 .setVisibility(View.INVISIBLE);
         	break;
         	
         case 5:
         	viewCor.setImageResource(R.drawable.img5);
-        	viewCor.setVisibility(View.VISIBLE);
-//    		viewIMG1 .setVisibility(View.INVISIBLE);
-//    		viewIMG2 .setVisibility(View.INVISIBLE);
-//    		viewIMG3 .setVisibility(View.INVISIBLE);
-//    		viewIMG4 .setVisibility(View.INVISIBLE);
-//    		viewIMG5 .setVisibility(View.VISIBLE);
-//    		viewIMG6 .setVisibility(View.INVISIBLE);
-//    		viewIMG7 .setVisibility(View.INVISIBLE);
-//    		viewIMG8 .setVisibility(View.INVISIBLE);        	
+        	viewCor.setVisibility(View.VISIBLE);        	
         	break;
         	
         case 6:
         	viewCor.setImageResource(R.drawable.img6);
         	viewCor.setVisibility(View.VISIBLE);
-//    		viewIMG1 .setVisibility(View.INVISIBLE);
-//    		viewIMG2 .setVisibility(View.INVISIBLE);
-//    		viewIMG3 .setVisibility(View.INVISIBLE);
-//    		viewIMG4 .setVisibility(View.INVISIBLE);
-//    		viewIMG5 .setVisibility(View.INVISIBLE);
-//    		viewIMG6 .setVisibility(View.VISIBLE);
-//    		viewIMG7 .setVisibility(View.INVISIBLE);
-//    		viewIMG8 .setVisibility(View.INVISIBLE);
         	break;
         	
         case 7:
         	viewCor.setImageResource(R.drawable.img7);
         	viewCor.setVisibility(View.VISIBLE);
-//    		viewIMG1 .setVisibility(View.INVISIBLE);
-//    		viewIMG2 .setVisibility(View.INVISIBLE);
-//    		viewIMG3 .setVisibility(View.INVISIBLE);
-//    		viewIMG4 .setVisibility(View.INVISIBLE);
-//    		viewIMG5 .setVisibility(View.INVISIBLE);
-//    		viewIMG6 .setVisibility(View.INVISIBLE);
-//    		viewIMG7 .setVisibility(View.VISIBLE);
-//    		viewIMG8 .setVisibility(View.INVISIBLE);
         	break;
         	
         case 8:
         	viewCor.setImageResource(R.drawable.img8);
-        	viewCor.setVisibility(View.VISIBLE);
-//    		viewIMG1 .setVisibility(View.INVISIBLE);
-//    		viewIMG2 .setVisibility(View.INVISIBLE);
-//    		viewIMG3 .setVisibility(View.INVISIBLE);
-//    		viewIMG4 .setVisibility(View.INVISIBLE);
-//    		viewIMG5 .setVisibility(View.INVISIBLE);
-//    		viewIMG6 .setVisibility(View.INVISIBLE);
-//    		viewIMG7 .setVisibility(View.INVISIBLE);
-//    		viewIMG8 .setVisibility(View.VISIBLE);        	
+        	viewCor.setVisibility(View.VISIBLE);        	
         	break;
         	
 		default:
@@ -189,115 +95,63 @@ public class TexturaActivity extends Activity
     {
         if(!wasExecuted){
 
-    		System.out.println("select coracao textura");
-        	texturas.texturas(coracao);
 
-//        	switch(view.getId()) {
-//        	
-//	        case R.id.imgBtn1:
-//	        	coracao = 1;
-//        		System.out.println("CORACAO1");
-//        		viewIMG1 .setVisibility(View.VISIBLE);
-//        		viewIMG2 .setVisibility(View.INVISIBLE);
-//        		viewIMG3 .setVisibility(View.INVISIBLE);
-//        		viewIMG4 .setVisibility(View.INVISIBLE);
-//        		viewIMG5 .setVisibility(View.INVISIBLE);
-//        		viewIMG6 .setVisibility(View.INVISIBLE);
-//        		viewIMG7 .setVisibility(View.INVISIBLE);
-//        		viewIMG8 .setVisibility(View.INVISIBLE);
-//	          break;
-//	          
-//	        case R.id.imgBtn2:
-//	        	coracao = 2;
-//        		System.out.println("CORACAO2");
-//        		viewIMG1 .setVisibility(View.INVISIBLE);
-//        		viewIMG2 .setVisibility(View.VISIBLE);
-//        		viewIMG3 .setVisibility(View.INVISIBLE);
-//        		viewIMG4 .setVisibility(View.INVISIBLE);
-//        		viewIMG5 .setVisibility(View.INVISIBLE);
-//        		viewIMG6 .setVisibility(View.INVISIBLE);
-//        		viewIMG7 .setVisibility(View.INVISIBLE);
-//        		viewIMG8 .setVisibility(View.INVISIBLE);
-//	          break;
-//	          
-//	        case R.id.imgBtn3:
-//	        	coracao = 3;
-//        		System.out.println("CORACAO3");
-//        		viewIMG1 .setVisibility(View.INVISIBLE);
-//        		viewIMG2 .setVisibility(View.INVISIBLE);
-//        		viewIMG3 .setVisibility(View.VISIBLE);
-//        		viewIMG4 .setVisibility(View.INVISIBLE);
-//        		viewIMG5 .setVisibility(View.INVISIBLE);
-//        		viewIMG6 .setVisibility(View.INVISIBLE);
-//        		viewIMG7 .setVisibility(View.INVISIBLE);
-//        		viewIMG8 .setVisibility(View.INVISIBLE);
-//	          break;
-//	          
-//	        case R.id.imgBtn4:
-//	        	coracao = 4;
-//        		System.out.println("CORACAO4");
-//        		viewIMG1 .setVisibility(View.INVISIBLE);
-//        		viewIMG2 .setVisibility(View.INVISIBLE);
-//        		viewIMG3 .setVisibility(View.INVISIBLE);
-//        		viewIMG4 .setVisibility(View.VISIBLE);
-//        		viewIMG5 .setVisibility(View.INVISIBLE);
-//        		viewIMG6 .setVisibility(View.INVISIBLE);
-//        		viewIMG7 .setVisibility(View.INVISIBLE);
-//        		viewIMG8 .setVisibility(View.INVISIBLE);
-//	          break;
-//	          
-//	        case R.id.imgBtn5:
-//	        	coracao = 5;
-//        		System.out.println("CORACAO5");
-//        		viewIMG1 .setVisibility(View.INVISIBLE);
-//        		viewIMG2 .setVisibility(View.INVISIBLE);
-//        		viewIMG3 .setVisibility(View.INVISIBLE);
-//        		viewIMG4 .setVisibility(View.INVISIBLE);
-//        		viewIMG5 .setVisibility(View.VISIBLE);
-//        		viewIMG6 .setVisibility(View.INVISIBLE);
-//        		viewIMG7 .setVisibility(View.INVISIBLE);
-//        		viewIMG8 .setVisibility(View.INVISIBLE);
-//	          break;
-//	          
-//	        case R.id.imgBtn6:
-//	        	coracao = 6;
-//        		System.out.println("CORACAO6");
-//        		viewIMG1 .setVisibility(View.INVISIBLE);
-//        		viewIMG2 .setVisibility(View.INVISIBLE);
-//        		viewIMG3 .setVisibility(View.INVISIBLE);
-//        		viewIMG4 .setVisibility(View.INVISIBLE);
-//        		viewIMG5 .setVisibility(View.INVISIBLE);
-//        		viewIMG6 .setVisibility(View.VISIBLE);
-//        		viewIMG7 .setVisibility(View.INVISIBLE);
-//        		viewIMG8 .setVisibility(View.INVISIBLE);
-//	          break;
-//	          
-//	        case R.id.imgBtn7:
-//	        	coracao = 7;
-//        		System.out.println("CORACAO7");
-//        		viewIMG1 .setVisibility(View.INVISIBLE);
-//        		viewIMG2 .setVisibility(View.INVISIBLE);
-//        		viewIMG3 .setVisibility(View.INVISIBLE);
-//        		viewIMG4 .setVisibility(View.INVISIBLE);
-//        		viewIMG5 .setVisibility(View.INVISIBLE);
-//        		viewIMG6 .setVisibility(View.INVISIBLE);
-//        		viewIMG7 .setVisibility(View.VISIBLE);
-//        		viewIMG8 .setVisibility(View.INVISIBLE);
-//	          break;
-//	          
-//	        case R.id.imgBtn8:
-//	        	coracao = 8;
-//        		System.out.println("CORACAO8");
-//        		viewIMG1 .setVisibility(View.INVISIBLE);
-//        		viewIMG2 .setVisibility(View.INVISIBLE);
-//        		viewIMG3 .setVisibility(View.INVISIBLE);
-//        		viewIMG4 .setVisibility(View.INVISIBLE);
-//        		viewIMG5 .setVisibility(View.INVISIBLE);
-//        		viewIMG6 .setVisibility(View.INVISIBLE);
-//        		viewIMG7 .setVisibility(View.INVISIBLE);
-//        		viewIMG8 .setVisibility(View.VISIBLE);
-//	          break;
-//	      }
+        	switch(view.getId()) {
+        	
+	        case R.id.textBtn1:
+	        	textura = 1;
+	        	texturas.texturas(coracao, textura);
+	          break;
+	          
+	        case R.id.textBtn2:
+	        	textura = 2;
+            	texturas.texturas(coracao, textura);
+	          break;
+	          
+	        case R.id.textBtn3:
+	        	textura = 3;
+            	texturas.texturas(coracao, textura);
+	          break;
+	          
+	        case R.id.textBtn4:
+	        	textura = 4;
+            	texturas.texturas(coracao, textura);
+	          break;
+	          
+	        case R.id.textBtn5:
+	        	textura = 5;
+            	texturas.texturas(coracao, textura);
+	          break;
+	          
+	        case R.id.textBtn6:
+	        	textura = 6;
+            	texturas.texturas(coracao, textura);
+	          break;
+	          
+	        case R.id.textBtn7:
+	        	textura = 7;
+            	texturas.texturas(coracao, textura);
+	          break;
+	          
+	        case R.id.textBtn8:
+	        	textura = 8;
+            	texturas.texturas(coracao, textura);
+	          break;
+	          
+	        case R.id.textBtn9:
+	        	textura = 9;
+	        	texturas.texturas(coracao, textura);
+	          break;
+	          
+	        case R.id.textBtn10:
+	        	textura = 10;
+	        	texturas.texturas(coracao, textura);
+	          break;
+	        	
+			default:
+				Toast.makeText(getApplicationContext(), "Textura não encontrada", Toast.LENGTH_LONG).show();
+				break;
+	      }
         }
     }
     
@@ -307,23 +161,4 @@ public class TexturaActivity extends Activity
         System.exit(0);
         getParent().finish();
     }
-    
-    
-//    private class CountDownTask extends AsyncTask<Void, Void, Void>
-//    {
-//        @Override
-//        protected Void doInBackground(Void... urls)
-//        {
-//        	
-//            for (int i = 0; i >= 0; --i)
-//            {
-//            	publishProgress(i);
-//            	SystemClock.sleep(1000);
-//            }
-//            main.putExtra("coracao", coracao);
-//            startActivity(main);
-//        	System.out.println("TERMINOU");
-//            return null;
-//        }
-//    }
 }
--- src/com/quatropdois/acaodocoracao/Texturas.java
+++ src/com/quatropdois/acaodocoracao/Texturas.java
@@ -1,9 +1,8 @@
 package com.quatropdois.acaodocoracao;
 
 import android.content.Context;
-import android.view.View;
-import android.widget.ImageButton;
-import android.widget.Toast;
+import android.util.Log;
+import android.widget.ImageView;
 
 public class Texturas {
 	
@@ -15,63 +14,468 @@ public class Texturas {
 		
 	}
 
-	public void texturas(int textura){
+	public void texturas(int coracao, int textura){
 //		int coracao = 0;
 		
-//		ImageButton viewCor = (ImageButton) textAct.findViewById(R.id.img_cor);
+		ImageView viewCor = (ImageView) textAct.findViewById(R.id.img_cor);
         
-    	switch(textura) {
+    	switch(coracao) {
     	
         case 1:
-//        	coracao = 1;
-//        	Toast.makeText(textAct, "coracao escolhido:" + coracao, Toast.LENGTH_LONG).show();
-    		System.out.println("CORACAO1");
+        	switch (textura) {
+			case 1:
+	        	viewCor.setImageResource(R.drawable.tex1_1);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+
+			case 2:
+	        	viewCor.setImageResource(R.drawable.tex1_2);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+
+			case 3:
+	        	viewCor.setImageResource(R.drawable.tex1_3);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+
+			case 4:
+	        	viewCor.setImageResource(R.drawable.tex1_5);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+				
+			case 5:
+	        	viewCor.setImageResource(R.drawable.tex1_6);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+				
+			case 6:
+	        	viewCor.setImageResource(R.drawable.tex1_7);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+				
+			case 7:
+	        	viewCor.setImageResource(R.drawable.tex1_8);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+
+			case 8:
+	        	viewCor.setImageResource(R.drawable.tex1_9);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+
+			case 9:
+	        	viewCor.setImageResource(R.drawable.tex1_10);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+
+			case 10:
+	        	viewCor.setImageResource(R.drawable.tex1_11);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+
+			default:
+				break;
+			}
           break;
           
         case 2:
-//        	coracao = 2;
-//        	Toast.makeText(textAct, "coracao escolhido:" + coracao, Toast.LENGTH_LONG).show();
+        	switch (textura) {
+			case 1:
+	        	viewCor.setImageResource(R.drawable.tex2_1);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
 
-    		System.out.println("CORACAO2");
+			case 2:
+	        	viewCor.setImageResource(R.drawable.tex2_2);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+
+			case 3:
+	        	viewCor.setImageResource(R.drawable.tex2_3);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+
+			case 4:
+	        	viewCor.setImageResource(R.drawable.tex2_4);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+				
+			case 5:
+	        	viewCor.setImageResource(R.drawable.tex2_5);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+				
+			case 6:
+	        	viewCor.setImageResource(R.drawable.tex2_6);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+				
+			case 7:
+	        	viewCor.setImageResource(R.drawable.tex2_7);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+
+			case 8:
+	        	viewCor.setImageResource(R.drawable.tex2_8);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+
+			case 9:
+	        	viewCor.setImageResource(R.drawable.tex2_9);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+
+			case 10:
+	        	viewCor.setImageResource(R.drawable.tex2_10);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+
+			default:
+				break;
+			}
           break;
           
         case 3:
-//        	coracao = 3;
-//        	Toast.makeText(textAct, "coracao escolhido:" + coracao, Toast.LENGTH_LONG).show();
-    		System.out.println("CORACAO3");
+        	switch (textura) {
+			case 1:
+	        	viewCor.setImageResource(R.drawable.tex3_1);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+
+			case 2:
+	        	viewCor.setImageResource(R.drawable.tex3_2);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+
+			case 3:
+	        	viewCor.setImageResource(R.drawable.tex3_3);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+
+			case 4:
+	        	viewCor.setImageResource(R.drawable.tex3_4);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+				
+			case 5:
+	        	viewCor.setImageResource(R.drawable.tex3_5);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+				
+			case 6:
+	        	viewCor.setImageResource(R.drawable.tex3_6);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+				
+			case 7:
+	        	viewCor.setImageResource(R.drawable.tex3_7);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+
+			case 8:
+	        	viewCor.setImageResource(R.drawable.tex3_8);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+
+			case 9:
+	        	viewCor.setImageResource(R.drawable.tex3_9);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+
+			case 10:
+	        	viewCor.setImageResource(R.drawable.tex3_10);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+
+			default:
+				break;
+			}
           break;
           
         case 4:
-//        	coracao = 4;
-//        	Toast.makeText(textAct, "coracao escolhido:" + coracao, Toast.LENGTH_LONG).show();
-    		System.out.println("CORACAO4");
+        	switch (textura) {
+			case 1:
+	        	viewCor.setImageResource(R.drawable.tex4_1);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+
+			case 2:
+	        	viewCor.setImageResource(R.drawable.tex4_2);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+
+			case 3:
+	        	viewCor.setImageResource(R.drawable.tex4_3);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+
+			case 4:
+	        	viewCor.setImageResource(R.drawable.tex4_4);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+				
+			case 5:
+	        	viewCor.setImageResource(R.drawable.tex4_5);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+				
+			case 6:
+	        	viewCor.setImageResource(R.drawable.tex4_6);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+				
+			case 7:
+	        	viewCor.setImageResource(R.drawable.tex4_7);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+
+			case 8:
+	        	viewCor.setImageResource(R.drawable.tex4_8);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+
+			case 9:
+	        	viewCor.setImageResource(R.drawable.tex4_9);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+
+			case 10:
+	        	viewCor.setImageResource(R.drawable.tex4_10);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+
+			default:
+				break;
+			}
           break;
           
         case 5:
-//        	coracao = 5;
-//        	Toast.makeText(textAct, "coracao escolhido:" + coracao, Toast.LENGTH_LONG).show();]
-    		System.out.println("CORACAO5");
+        	switch (textura) {
+			case 1:
+	        	viewCor.setImageResource(R.drawable.tex5_1);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+
+			case 2:
+	        	viewCor.setImageResource(R.drawable.tex5_2);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+
+			case 3:
+	        	viewCor.setImageResource(R.drawable.tex5_3);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+
+			case 4:
+	        	viewCor.setImageResource(R.drawable.tex5_4);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+				
+			case 5:
+	        	viewCor.setImageResource(R.drawable.tex5_5);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+				
+			case 6:
+	        	viewCor.setImageResource(R.drawable.tex5_6);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+				
+			case 7:
+	        	viewCor.setImageResource(R.drawable.tex5_7);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+
+			case 8:
+	        	viewCor.setImageResource(R.drawable.tex5_8);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+
+			case 9:
+	        	viewCor.setImageResource(R.drawable.tex5_9);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+
+			case 10:
+	        	viewCor.setImageResource(R.drawable.tex5_10);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+
+			default:
+				break;
+			}
           break;
           
         case 6:
-//        	coracao = 6;
-//        	Toast.makeText(textAct, "coracao escolhido:" + coracao, Toast.LENGTH_LONG).show();
+        	switch (textura) {
+			case 1:
+	        	viewCor.setImageResource(R.drawable.tex6_1);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
 
-    		System.out.println("CORACAO6");
+			case 2:
+	        	viewCor.setImageResource(R.drawable.tex6_2);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+
+			case 3:
+	        	viewCor.setImageResource(R.drawable.tex6_3);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+
+			case 4:
+	        	viewCor.setImageResource(R.drawable.tex6_4);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+				
+			case 5:
+	        	viewCor.setImageResource(R.drawable.tex6_5);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+				
+			case 6:
+	        	viewCor.setImageResource(R.drawable.tex6_6);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+				
+			case 7:
+	        	viewCor.setImageResource(R.drawable.tex6_7);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+
+			case 8:
+	        	viewCor.setImageResource(R.drawable.tex6_8);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+
+			case 9:
+	        	viewCor.setImageResource(R.drawable.tex6_9);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+
+			case 10:
+	        	viewCor.setImageResource(R.drawable.tex6_10);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+
+			default:
+				break;
+			}
           break;
           
         case 7:
-//        	coracao = 7;
-//        	Toast.makeText(textAct, "coracao escolhido:" + coracao, Toast.LENGTH_LONG).show();
-    		System.out.println("CORACAO7");
+        	switch (textura) {
+			case 1:
+	        	viewCor.setImageResource(R.drawable.tex7_1);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+
+			case 2:
+	        	viewCor.setImageResource(R.drawable.tex7_2);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+
+			case 3:
+	        	viewCor.setImageResource(R.drawable.tex7_3);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+
+			case 4:
+	        	viewCor.setImageResource(R.drawable.tex7_4);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+				
+			case 5:
+	        	viewCor.setImageResource(R.drawable.tex7_5);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+				
+			case 6:
+	        	viewCor.setImageResource(R.drawable.tex7_6);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+				
+			case 7:
+	        	viewCor.setImageResource(R.drawable.tex7_7);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+
+			case 8:
+	        	viewCor.setImageResource(R.drawable.tex7_8);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+
+			case 9:
+	        	viewCor.setImageResource(R.drawable.tex7_9);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+
+			case 10:
+	        	viewCor.setImageResource(R.drawable.tex7_10);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+
+			default:
+				break;
+			}
           break;
           
         case 8:
-//        	coracao = 8;
-//        	Toast.makeText(textAct, "coracao escolhido:" + coracao, Toast.LENGTH_LONG).show();
+        	switch (textura) {
+			case 1:
+	        	viewCor.setImageResource(R.drawable.tex8_1);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
 
-    		System.out.println("CORACAO8");
-          break;
+			case 2:
+	        	viewCor.setImageResource(R.drawable.tex8_2);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+
+			case 3:
+	        	viewCor.setImageResource(R.drawable.tex8_3);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+
+			case 4:
+	        	viewCor.setImageResource(R.drawable.tex8_4);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+				
+			case 5:
+	        	viewCor.setImageResource(R.drawable.tex8_5);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+				
+			case 6:
+	        	viewCor.setImageResource(R.drawable.tex8_6);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+				
+			case 7:
+	        	viewCor.setImageResource(R.drawable.tex8_7);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+
+			case 8:
+	        	viewCor.setImageResource(R.drawable.tex8_8);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+
+			case 9:
+	        	viewCor.setImageResource(R.drawable.tex8_9);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+
+			case 10:
+	        	viewCor.setImageResource(R.drawable.tex8_10);
+	    		Log.i("TEXTURA", "TEXTURA:" + textura);				
+				break;
+
+			default:
+				break;
+			}
+          break;      
       }
 	}
 }
