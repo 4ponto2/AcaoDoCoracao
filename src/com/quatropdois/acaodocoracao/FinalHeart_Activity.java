9494791348023ef58d099e13df7f20ebdbb09064
Ismael <Ismael@192.168.0.109>
2014-06-25 10:17:35 -0300

    salva foto

--- src/com/quatropdois/acaodocoracao/FinalHeart_Activity.java
+++ src/com/quatropdois/acaodocoracao/FinalHeart_Activity.java
@@ -1,13 +1,24 @@
 package com.quatropdois.acaodocoracao;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.ShareActionProvider;
+import android.widget.Toast;
 
 public class FinalHeart_Activity extends Activity {
 	
@@ -23,6 +34,8 @@ private boolean wasExecuted;
 	
 	public ImageView viewCor, 
 					 viewSent;
+	
+	private Bitmap myHeart;
 	
 	private Final_textura_palabra texturas_final;
 
@@ -80,6 +93,9 @@ private boolean wasExecuted;
     	texturas_final.Sentimento(sentimento);
     	viewSent.setVisibility(View.VISIBLE);
     	
+    	BitmapDrawable drawable = (BitmapDrawable) viewCor.getDrawable();
+    	myHeart = drawable.getBitmap();
+
     }
 
     public void onBackPressed(){
@@ -89,4 +105,38 @@ private boolean wasExecuted;
         getParent().finish();
     }
 
+    
+    public void onResume(){
+    	SalvaBit(myHeart);    	
+    	super.onResume();
+    }
+    
+    
+    protected void SalvaBit(final Bitmap result) 
+    { 
+    	String PATH = Environment.getExternalStorageDirectory().toString();
+    	if(result!=null){
+    		// TODO Auto-generated method stub
+			OutputStream outStream = null;
+			File file = new File(PATH, "sampleimage4.jpg");
+			Log.i("LOCAL", "LOCAL: " + file);
+			try{
+				outStream = new FileOutputStream(file);
+				result.compress(Bitmap.CompressFormat.JPEG, 100, outStream);
+				outStream.flush();
+				outStream.close();
+				Toast.makeText(FinalHeart_Activity.this, "Saved", Toast.LENGTH_LONG).show();
+				
+			}catch (FileNotFoundException e){
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				
+			}catch (IOException e){
+//				TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+       
+    }   
+    
 }
