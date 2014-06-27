1e7f91601777595662ccd09e407a61f9d4b9887e
Marcio Lorran <fealaure@hotmail.com>
2014-06-27 16:27:35 -0300

    Correção de erro não abria app

--- src/com/quatropdois/acaodocoracao/MainActivity.java
+++ src/com/quatropdois/acaodocoracao/MainActivity.java
@@ -4,10 +4,7 @@ package com.quatropdois.acaodocoracao;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
 
 public class MainActivity extends Activity
 {
--- src/com/quatropdois/acaodocoracao/Sentimento_activity.java
+++ src/com/quatropdois/acaodocoracao/Sentimento_activity.java
@@ -1,23 +1,12 @@
 package com.quatropdois.acaodocoracao;
 
 import android.app.Activity;
-import android.app.ActionBar;
-import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.MotionEvent;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.Toast;
-import android.os.Build;
 
 public class Sentimento_activity extends Activity {
 	
