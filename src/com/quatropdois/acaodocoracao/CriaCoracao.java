5d18aa6a85481c9700e94663a59362701b65faad
Marcio Lorran <fealaure@hotmail.com>
2014-06-30 15:05:42 -0300

    sdfsd

--- src/com/quatropdois/acaodocoracao/CriaCoracao.java
+++ src/com/quatropdois/acaodocoracao/CriaCoracao.java
@@ -4,22 +4,32 @@ import java.util.Timer;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.widget.VideoView;
 
 public class CriaCoracao extends Activity {
 	
 	VideoView video;
 	Intent changeOnTime;
-	
+	int ii;
 	Timer t = new Timer();
+	
+	private int sentimento = 0;
+	
+	private int coracao = 0;
+	
+	private int textura = 0;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_cria_coracao);
+
+		changeOnTime = new Intent(CriaCoracao.this, Tela_Final.class);
 		
 		video = (VideoView) findViewById(R.id.vidCoracao);
 		String urlpath = "android.resource://" + getPackageName() + "/" + R.raw.costura_1;
@@ -28,16 +38,44 @@ public class CriaCoracao extends Activity {
 		
 		video.start();
 		
-		new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                final Intent changeOnTime = new Intent(CriaCoracao.this, Tela_Final.class);
-                CriaCoracao.this.startActivity(changeOnTime);
-                CriaCoracao.this.finish();
-            }
-        }, 5000);
+        Intent iin = getIntent();
+        Bundle extras = iin.getExtras();
+        coracao = (int) extras.getInt("coracao");
+        textura = (int) extras.getInt("textura");
+        sentimento = (int) extras.getInt("sentimento");
+		
+		ii = video.getDuration();
+		
+
+		video.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+			
+			@Override
+			public void onCompletion(MediaPlayer mp) {
+				// TODO Auto-generated method stub
+				Log.i("VIDE", "FIM");
+	        	changeOnTime.putExtra("coracao", coracao);
+	        	changeOnTime.putExtra("textura", textura);
+	        	changeOnTime.putExtra("sentimento", sentimento);
+				startActivity(changeOnTime);
+				CriaCoracao.this.finish();
+				
+			}
+		});
+		
+//		for (int i=0; i>ii; i++){
+//			Log.i("teste", "teste");
+//		}
+		
+//		new Handler().postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                final Intent changeOnTime = new Intent(CriaCoracao.this, Tela_Final.class);
+//                CriaCoracao.this.startActivity(changeOnTime);
+//                CriaCoracao.this.finish();
+//            }
+//        }, 5000);
 		
 	}
+
 }
-	
 	
